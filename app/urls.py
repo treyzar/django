4e3437e2f8496808746d286e8bb0b0071035eb9d
StudentBI8 <sharafaskar@gmail.com>
2024-12-27 14:23:11 +0300

    add search

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('create', BookCreateView.as_view(), name='create'),
     path('delete/<int:pk>', BookDeleteView.as_view(), name='delete'),
     path('update/<int:pk>', BookUpdateView.as_view(), name='update'),
+    path('books/', BookListView.as_view(), name='book-list'),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Book
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
 
 
 class BookListView(ListView):
@@ -10,6 +12,14 @@ class BookListView(ListView):
     template_name = 'book_list.html'
     context_object_name = 'books'
 
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            # Ищем по названию и автору (регистр не учитывается)
+            return Book.objects.filter(
+                Q(title__icontains=query) | Q(author__icontains=query)
+            )
+        return Book.objects.all()
 class BookCreateView(CreateView):
     model = Book
     fields = '__all__'
