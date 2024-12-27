7059059230dfc7d45eee78d788774149054557cc
EggZys <153300517+EggZys@users.noreply.github.com>
2024-12-27 11:49:51 +0300

    первый коммит

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('', BookListView.as_view(), name = 'main'),
     path('create', BookCreateView.as_view(), name='create'),
     path('delete/<int:pk>', BookDeleteView.as_view(), name='delete'),
-    path('update/<int:pk>', BookUpdateView.as_view(), name='update')
+    path('update/<int:pk>', BookUpdateView.as_view(), name='update'),
 ]
